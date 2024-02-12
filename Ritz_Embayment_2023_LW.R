f30766fff09ac52a885eb7d54048ceba8eac4d94
Thornton Ritz <61156565+tritz18@users.noreply.github.com>
2024-02-12 09:32:21 -0500

    Added linear model R file for figure 4 and table 1 generation

---
+++ Ritz_Embayment_2023_LW.R
@@ -0,0 +1,88 @@
+################################################################################
+####              Fish Dissection Data                                         #### 
+####              Flynn Bay, Eel Bay, Clayton, NY completed by TR           ####               
+#################################################################################
+
+#### Packages used for the following data preparation and analysis ####
+library(dplyr)
+library(ggpubr)
+library(zoo)
+library(tidyverse)
+library(lubridate)
+library(stats)
+library(broom)
+library(ggpubr)
+library(readxl)
+library(writexl)
+library(tidymodels)
+library(ggpmisc)
+install.packages(groupedstats)
+options(scipen=999, digits = 2) 
+
+##################
+
+###############################################################################################################################
+################                                             ###############
+###############################################################################################################################
+
+#### set working directory and make sure dissection data file in order ####
+
+setwd("/Users/thorn/OneDrive/Desktop/Ritz_Embayment_2023/Manuscript_Data_Files/")
+LW_YOY_Raw<- read_excel("LW_YOY_2021.xlsx")
+LW_YOY_Raw$Date<-as.POSIXct(LW_YOY_Raw$Date, format="%Y-%m-%d")
+LW_YOY_Raw$Species<- as.factor(LW_YOY_Raw$Species) 
+LW_YOY_Raw$Location<- as.factor(LW_YOY_Raw$Location)
+
+#### Filter species of interest and create study data variable 1-Start of sampling ##
+LW_YOY<- LW_YOY_Raw |>
+  select(-Notes)|>
+  arrange(Date) |>
+  group_by(Date) |>
+  mutate(Study_Day= cur_group_id()) |>
+  filter(Species %in% c("ICTALURID", "CARP", "CYPRINIDAE", "KILLI", 
+                        "LEPOMIS", "LMB", "SMB")) |>
+  mutate(K = Weight/(Length^3) * 100,0000) |>
+  filter(Study_Day <=42)
+
+#### Linear model with total length ~ study date ####
+
+LW_YOY_SUM<- LW_YOY |>
+  group_by(Species, Study_Day, Location) |>
+  summarise(Mean_Length=mean(Length), Mean_Weight=mean(Weight))
+
+shapiro.test(LW_YOY$Length)
+
+
+ggplot(LW_YOY, aes(log(Length)))+
+  geom_histogram(bins=20)+
+  facet_wrap(~Species)
+
+Model_LM<- LW_YOY |>
+  group_by(Location, Species) |>
+  mutate(Log_length=log(Length), Log_weight=log(Weight)) |>
+  group_modify(~ broom::tidy(lm(Study_Day ~ Log_length, data = .x)))
+
+
+LW_YOY$Date<-as.Date(LW_YOY$Date,format="%M-%d")
+
+ggplot(LW_YOY, aes(Date, log(Length), group=Location, color=Location, 
+                   lty=Location,fill=Location))+
+  geom_point()+
+  geom_smooth(method="lm")+ scale_color_manual(values=c("#003366", "#3366CC"))+
+  scale_fill_manual(values=c("#003366", "#3366CC"))+
+  theme_bw()+
+  theme(legend.position = c(.88,.20), legend.text = element_text(size=14), legend.title = element_text(size = 15),
+        axis.text = element_text(size=13),
+         axis.title = element_text(size=14),
+         strip.text = element_text(size=14), axis.title.x = element_blank(),
+        strip.background = element_rect (fill = "white"),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
+  stat_poly_eq(use_label(c("R2", "p"))) +ylab("Log Total Length (mm)")+
+  scale_x_date(date_breaks = "14 days", 
+               minor_breaks = "7 days",
+               date_labels = "%b %d",
+               limits=as.Date(c("2021-06-11","2021-07-22")))+
+  scale_y_continuous(breaks = seq(2,5,0.5), limits=c(2,5))+
+  facet_wrap(~Species, nrow=2, scales="free_x")
+ggsave("Figure_4.png", dpi=300, height=8, width=12)
+
